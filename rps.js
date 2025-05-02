4a2d0f8b115cb0809073366ef13a523c9cf15cac
DarkFerrari <171889029+DarkFerrari@users.noreply.github.com>
2025-05-01 20:46:24 -0500

    Rps UI (#5)
    * add button functionality
    
    add button functionality including the input/submit button
    
    * add overall game winner
    
    add game count of 5 for each round and overall game winner display

--- rps.js
+++ rps.js
@@ -1,125 +1,161 @@
 console.log("Hello there");
-let HumanScore = 0
-let ComputerScore = 0
-let tie = 0
+let HumanScore = 0;
+let ComputerScore = 0;
+let tie = 0;
+let GameResult = "";
+let gameCount=0;
+let displayGameCount
+let displayOutput
+
+document.querySelector(".play-again").addEventListener("click", ()=>{
+    HumanScore = 0;
+    ComputerScore = 0;
+    tie = 0;
+    GameResult = "";
+    gameCount=0;
+    if (displayOutput.contains(displayGameCount)) {
+        displayOutput.removeChild(displayGameCount);
+      }
+    console.log("remove the child")
+    updateScore()
+} )
 
 function getComputerChoice() {
-    let a = (Math.random() * 100);
-    //console.log(a);
+  let computerChoice = Math.random() * 100;
 
-    if (a<=33){
-        //console.log("Rock")
-        a="rock"
-    }
-    else if (a>66){
-        //console.log("scissor")
-        a="scissor"
-    }
-    else{
-        //console.log("paper")
-        a="paper"
-    }
+  if (computerChoice < 33.33) {
+    //console.log("Rock")
+    computerChoice = "rock";
+  } else if (computerChoice > 66.66) {
+    //console.log("scissor")
+    computerChoice = "scissor";
+  } else {
+    //console.log("paper")
+    computerChoice = "paper";
+  }
 
-    return a;
+  return computerChoice;
 }
 
-function getHumanChoice(){
+function getHumanChoice(a) 
+{
+  if (a == "") {
+     return null;
+  } else {
+    b = a.toLowerCase();
+  }
+  console.log("Provided input is '", b, "' and it is of type ", typeof b);
+  console.log("ParseInt of the ", b, " is : ", parseInt(b));
 
-    let a = prompt("enter rock/paper/scissor"); 
-    if (a==null){
-        return null
-    }
-    let b = a.toLowerCase();
-    console.log("Provided input is '", b,"' and it is of type ",typeof b);
-    console.log("ParseInt of the ",b, " is : ",parseInt(b));
-    
-    //let checked_value = check_valid_string(b)
-
-    
-    return check_valid_string(b)
+  return check_valid_string(b);
 }
 
-function check_valid_string(b){
-    let result = '';
-    if (((b=="rock")||(b=="paper")||(b=="scissor"))) //if input is string, parseInt tries to convert to number and gives NaN, which allows to enter "IF"
-        {
-            result = b;
-        }
-        else if (b===null) return null;
-       else// if isNaN is flase, it means the value is a number, which gives should ask the user to only enter rock,paper,scissor 
-       {
-        alert ("You entered wrong value, enter ONLY rock paper scissor");
-        result = getHumanChoice()
-        console.log("you entered wrong value, enter only rock paper scissor")
-        // let b = prompt("enter only rock paper scissor")
-        // if (b==null) return null;
-        // let c = b.toLowerCase();
-        // console.log("value of lower case c is : ",c)
-        // result = check_valid_string(c)
-       }
-    return result;
-
+function check_valid_string(b) {
+  let result = "";
+  if (b == "rock" || b == "paper" || b == "scissor") {
+    result = b;
+  }
+  return result;
 }
 
 // let Human = getHumanChoice();
 
-
 // let Computer = getComputerChoice();
 
-
-
 function beginGame(Human, Computer) {
-    if (Human === Computer) {
-        return "tie";
-    } else if (Computer === "rock") {
-        if (Human === "paper") return "Human wins";
-        else if (Human === "scissor") return "Computer wins";
-    } else if (Computer === "scissor") {
-        if (Human === "rock") return "Human wins";
-        else return "Computer wins";
-    } else if (Computer === "paper") {
-        if (Human === "scissor") return "Human wins";
-        else return "Computer wins";
-    } else {
+  if (Human === Computer) {
+    return "tie";
+  } else if (Computer === "rock") {
+        if (Human === "paper") return "Human wins this round";
+        else if (Human === "scissor") return "Computer wins this round";
+  } else if (Computer === "scissor") {
+        if (Human === "rock") return "Human wins this round";
+        else return "Computer wins this round";
+  } else if (Computer === "paper") {
+        if (Human === "scissor") return "Human wins this round";
+        else return "Computer wins this round";
+  } else {
         return "something wrong";
-    }
-}
-
-
-//GameResult=beginGame(Human,Computer)
-function playRound() {
-  for(i=0;i<5;i++) {
-    Human = getHumanChoice();
-    if (Human==null) break;
-    console.log("Human choice is ",Human);
-    Computer = getComputerChoice();
-    console.log("Computer choice is ",Computer);
-    GameResult = beginGame(Human, Computer);
-    console.log(GameResult)
-    if (GameResult == "tie") {
-        tie++
-    }
-    else if (GameResult == "Computer wins") ComputerScore ++;
-    else HumanScore ++;
   }
-  return GameResult
+
 }
 
+function playRound(Human) 
+{
+  if (Human == "") {
+    return GameResult = null;
+  }
+  console.log("Human choice is : ", Human);
+  Computer = getComputerChoice();
+  console.log("computer choice is : ", Computer);
+  GameResult = beginGame(Human, Computer);
+  gameCount++
+  if (GameResult == "tie") {
+    tie++;
+  } else if (GameResult == "Computer wins this round") ComputerScore++;
+  else HumanScore++;
 
 
+  updateScore();
+  return GameResult;
+}
 
+let input = document.querySelector(".rps-input");
+let submit = document.querySelector(".submit");
+let Human = null;
 
-GameResult = playRound()
-if (HumanScore==ComputerScore) GameResult="Round Tie"
-    else if (HumanScore>ComputerScore) GameResult= ("Human wins this round ")
-    else GameResult=("computer wins this round")
+document.querySelector(".rock-button").addEventListener("click", () => playRound("rock"))
+document.querySelector(".paper-button").addEventListener("click", () => playRound("paper"))
+document.querySelector(".scissor-button").addEventListener("click", () => playRound("scissor"))
 
-document.getElementsByClassName("output")[0].innerHTML = GameResult + "<br>Number of tie is "+tie+"<br> <b>Human score is</b> " +HumanScore + " <br>Computer score is " +ComputerScore
+submit.addEventListener("click", () => 
+    {
+        a = input.value;
+        let Human = getHumanChoice(a);
+        if(Human==null) return document.querySelector(".output").textContent = "no choice entered";
+        GameResult = playRound(Human);
+        console.log("from eventlistener",GameResult);
 
+        //console.log(GameResult)
 
+        // if (GameResult == "tie") GameResult = "Round Tie";
+        // else if (GameResult == "Human wins this round") GameResult = "Human wins this round";
+        // else GameResult = "computer wins this round";
 
+        updateScore()
+    }
+);
 
+    function updateScore()
+    {   
+        console.log(GameResult)
+        displayOutput = document.querySelector(".output")
+        displayOutput.textContent =
+        GameResult +
+        "\n\nOverall number of tie's is " +
+        tie +
+        "\nOverall Human score is " +
+        HumanScore +
+        " \nOverall Computer score is " +
+        ComputerScore;
+        document.querySelector(".output").style.whiteSpace = "pre-line";
 
+        if (gameCount==5) 
+            {
+                displayGameCount = document.createElement("p")
+                displayGameCount.textContent = "The overall game winner is: "+  
+                    ( HumanScore > ComputerScore? "Human" :
+                        ComputerScore > HumanScore? "Computer": "TIE"
+                    )
+                displayOutput.appendChild(displayGameCount)
+                
+            }   
+        else if(gameCount==6) 
+            {
+                const clickEvent= new Event("click");
+                document.querySelector(".play-again").dispatchEvent(clickEvent)
+            }
+    }
 
 
 //rock wins with scissor so rock but paper
